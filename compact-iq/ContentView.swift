831e0aa1c5738279ea2ac645c7f057289a7c8ff1
Rajeev J <rajeev.jamaiyar@ninebit.in>
2024-04-22 17:43:07 +0530

    hello world - first commit

---
+++ compact-iq/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  compact-iq
+//
+//  Created by Surbhi Karmahe Jamaiyar on 20/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world 123!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ compact-iq/compact_iqApp.swift
@@ -0,0 +1,17 @@
+//
+//  compact_iqApp.swift
+//  compact-iq
+//
+//  Created by Surbhi Karmahe Jamaiyar on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct compact_iqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
